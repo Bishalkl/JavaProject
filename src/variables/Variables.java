53501368cb487dcd5839b87cb89ab56814b0bae0
Bishalkl <bishal123koirala@gmail.com>
2023-09-12 23:44:30 +0545

    Initial commit

---
+++ src/variables/Variables.java
@@ -0,0 +1,11 @@
+package variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        int a = 12;
+        System.out.println(a);
+        }
+
+    }
+
